e3c06f72395ec5cd8c61a3935ed4f3ec0300188a
sem1103 <sem.sem.geo.az1998@mail.ru>
2024-09-10 22:10:43 +0400

    fix design

--- Desktop/shrf-main/vite.config.js
+++ Desktop/shrf-main/vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 8080
+export default defineConfig(({})=>{
+  const baseUrl=process.env.VITE_API_BASE_URL;
+  return {
+    plugins: [react()],
+    server:{
+      port:8080
+    },
+    define:{
+      'process.env.VITE_API_BASE_URL':JSON.stringify(baseUrl)
+    }
   }
 })
